8d0469c1f01d3cf403970d24e0ad5a9506c3ae37
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-08-09 10:32:45 -0700

    Rhizome: Appease dartfmt

--- lib/src/rhizome.dart
+++ lib/src/rhizome.dart
@@ -2,16 +2,14 @@ import 'continuum.dart';
 import 'repertory.dart';
 
 /// A living distribution of all the world's information.
-/// 
+///
 /// The Rhizome class provides the primary API for storing information,
 /// retrieving a specific information object (a [Thing]), and seeking
 /// information.
 class Rhizome {
-  
   final Continuum continuum;
   final Repertory repertory;
 
   Rhizome(
-    {this.continuum = const Continuum(), this.repertory = const Repertory()});
-
+      {this.continuum = const Continuum(), this.repertory = const Repertory()});
 }
