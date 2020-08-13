04ddf80a4ce5ba2e2f908d87f3793a804fb7d5af
Yong Bakos <ybakos@humanoriented.com>
2020-08-13 00:28:46 -0700

    Repertory: Add doc comments.

--- lib/src/repertory.dart
+++ lib/src/repertory.dart
@@ -8,17 +8,20 @@ import 'thing.dart';
 class Repertory {
 
   final Map<dynamic, Uri> _index;
+  /// A key-value store mapping information to URIs of [Thing]s.
   Map<dynamic, Uri> get index => _index;
 
   Repertory({index}) : _index = index ?? <dynamic, Uri>{};
 
+  /// Retrieve the Uri of a [Thing] that represents `information`.
   Uri seek(dynamic information) {
     return _index[information];
   }
   
+  /// Add a [Thing] to the repertory.
   void register(Thing thing) {
     if (_index.containsKey(thing.information)) return;
     _index[thing.information] = thing.uri;
   }
-  
+
 }
